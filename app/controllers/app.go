133ca17b2868541bf47f3184b4ead793e730960b
Tim Weber <scy@scy.name>
2014-07-02 08:49:38 +0200

    Extract entriesToJson()

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,19 +11,26 @@ type App struct {
 
 type stringMap map[string]interface{}
 
-func (c App) Index() revel.Result {
-	entries := doohan.FetchEntries()
+func entryToJson(entry doohan.Entry) stringMap {
+	json := make(stringMap, 10)
+	json["id"] = entry.ID
+	json["start"] = entry.Start.UnixNano()
+	if entry.Stop.Valid {
+		json["stop"] = entry.Stop.Time.UnixNano()
+		json["duration"] = entry.Stop.Time.Sub(entry.Start).Nanoseconds()
+	}
+	json["running"], json["description"] = entry.Running, entry.Description
+	return json
+}
+
+func entriesToJson(entries []doohan.Entry) []stringMap {
 	var jsons []stringMap
 	for _, entry := range entries {
-		json := make(stringMap, 10)
-		json["id"] = entry.ID
-		json["start"] = entry.Start.UnixNano()
-		if entry.Stop.Valid {
-			json["stop"] = entry.Stop.Time.UnixNano()
-			json["duration"] = entry.Stop.Time.Sub(entry.Start).Nanoseconds()
-		}
-		json["running"], json["description"] = entry.Running, entry.Description
-		jsons = append(jsons, json)
+		jsons = append(jsons, entryToJson(entry))
 	}
-	return c.RenderJson(jsons)
+	return jsons
+}
+
+func (c App) Index() revel.Result {
+	return c.RenderJson(entriesToJson(doohan.FetchEntries()))
 }
